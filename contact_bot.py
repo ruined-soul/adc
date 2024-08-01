1e4f3c75ac7b7d893d1a0bd93a8e3a7b3ff7979d
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-01 09:13:14 +0530

    Update contact_bot.py

--- contact_bot.py
+++ contact_bot.py
@@ -10,7 +10,6 @@ api_id = '2170492'
 api_hash = '82b683da442942d5c177ec520318a32f'
 bot_token = '7304879730:AAHWnILVrNQjeD7QuLMd3UOuC5xf72mzd5I'
 admin_chat_id = '1159381624'
-
 app = Client("contact_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.private & ~filters.bot)
@@ -32,6 +31,15 @@ async def reply_to_user(client, message):
         original_sender_id = int(message.reply_to_message.text.split("From: ")[-1])
         logging.info(f"Sending reply from admin {admin_chat_id} to user {original_sender_id}")
         await client.send_message(original_sender_id, message.text)
+
+@app.on_message(filters.command("start") & filters.private)
+async def start_command(client, message):
+    welcome_message = (
+        "Hello! I'm personal bot of my @ruined_soul.\n"
+        "Please send your message, and I will forward it to the master.\n"
+        "If you have any questions or need support, let me know!"
+    )
+    await message.reply_text(welcome_message)
 
 if __name__ == "__main__":
     app.run()
